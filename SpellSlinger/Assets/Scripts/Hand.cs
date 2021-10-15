a57b0a7096c1460e4460fe9b0b4fb1d6e114ed38
Drilon Bajrami <drilonbajrami@hotmail.com>
2021-10-15 15:06:26 +0200

    Steam VR Plugin Implemented
    SteamVR Plugin has lots of files, I am not sure if we need almost 99% of them so we might delete later on.

---
+++ SpellSlinger/Assets/Scripts/Hand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Hand
+{
+    //Containing class for incoming hand data
+    public string name;
+    public Bone[] bones;
+    public int side;
+    public string timecode;
+    public string poseName;
+    public int poseID;
+    public bool poseActive;
+    public float poseConf;
+}
+
+[Serializable]
+public class Bone {
+    public string name;
+    public int parent;
+    public string[] translation;
+    public string[] rotation;
+    public string[] scale;
+    public string[] pre_rotation;
+    public string[] post_rotation;
+    public string rotation_order;
+}
